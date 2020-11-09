830e044c0f3fcde478b1eea96d111d033358e0b1
Tragikomedia <62154148+Tragikomedia@users.noreply.github.com>
2020-11-09 12:30:44 +0100

    Delete Manifest.java

--- android/app/src/profile/gen/com/flutter_buddies/flutter_buddies_destini/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.flutter_buddies.flutter_buddies_destini;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
